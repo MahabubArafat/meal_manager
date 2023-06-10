39f9ed6827888616ed345fb359abb98ac30f46da
MahabubArafat <arafatmahabub37@gmail.com>
2023-06-10 10:29:29 +0600

    db cluster created

--- routes/api/student_profile.js
+++ routes/api/student_profile.js
@@ -2,3 +2,15 @@ const express = require("express");
 const { check, validationResult } = require("express-validator");
 
 const StudentProfile = require("../../models/StudentProfile");
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  try {
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const port = 3000;
 
 app.use(express.static("public"));
 
+app.use("/api/student_profile", require("./routes/api/student_profile"));
+
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${port}`);
 });
