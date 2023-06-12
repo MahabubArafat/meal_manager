194ad8c8bddd612e93d415673c030902b7d90fe5
MahabubArafat <arafatmahabub37@gmail.com>
2023-06-12 09:28:32 +0600

    error check added

--- routes/api/student_profile.js
+++ routes/api/student_profile.js
@@ -7,11 +7,11 @@ const router = express.Router();
 
 router.post(
   "/",
-  check("name", "Provide Your Full Name"),
-  check("studentID", "Student ID is required"),
-  check("email", "Provide your CUET mail"),
-  check("phoneNumber", "Phone Number is required"),
-  check("roomNumber", "Room Number is required"),
+  check("name", "Provide Your Full Name").not().isEmpty(),
+  check("studentID", "Student ID is required").not().isEmpty(),
+  check("email", "Provide your CUET mail").isEmail(),
+  check("phoneNumber", "Phone Number is required").not().isEmpty(),
+  check("roomNumber", "Room Number is required").not().isEmpty(),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
