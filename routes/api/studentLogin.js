ac3665132ea2053e552ed6b15196d0963bde915b
MahabubArafat <arafatmahabub37@gmail.com>
2023-08-25 19:44:59 +0600

    student login added

--- routes/api/studentLogin.js
+++ routes/api/studentLogin.js
@@ -24,14 +24,48 @@ router.get("/", async (req, res) => {
 // @description Authorization for student
 // @access      Public
 router.post(
-  "/login",
+  "/",
   [
     check("studentID", "Please Enter Your Student ID").not().isEmpty(),
     check("pin", "Please Enter Your pin").not().isEmpty(),
   ],
   async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { studentID, pin } = req.body;
+
     try {
-      res.send("auth route");
+      let user = await StudentProfile.findOne({ studentID });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+      const isPinMatch = await bcrypt.compare(pin, user.pin);
+      if (!isPinMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      //* studentID and Pin match, so return the jwt
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecretToken"),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
