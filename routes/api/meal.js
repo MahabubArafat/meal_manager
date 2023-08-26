136a79b11712dd7d4484cc85ec94db76b10e2664
MahabubArafat <arafatmahabub37@gmail.com>
2023-08-27 00:02:10 +0600

    meal route added

---
+++ routes/api/meal.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+const studentMiddleware = require("../../middleware/studentMiddleware");
+
+const Student = require("../../models/StudentProfile");
+const Transaction = require("../../models/Transactions");
+const Manager = require("../../models/Manager");
+
+// @route       GET api/meal
+// @description Get all meals taken by this user
+// @access      Private
+router.get("/", studentMiddleware, async (req, res) => {
+  try {
+    res.send("meal route");
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server");
+  }
+});
+
+module.exports = router;
