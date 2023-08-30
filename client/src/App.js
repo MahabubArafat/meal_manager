d346d0da9019c224b4e64ebe45352a783114fb3e
MahabubArafat <arafatmahabub37@gmail.com>
2023-08-30 21:01:38 +0600

    landing and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import QRScannerComponent from "./components/QRscannerComponent";
+import QRScannerComponent from "./components/auth/QRscannerComponent";
+import Navbar from "./components/layout/Navbar";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 
 const App = () => {
   return (
     <>
+      <Navbar />
+      <Routes></Routes>
       <div className="container">
         <Routes>
+          <Route path="/" element={<h1>Welcome To Meal Manager</h1>} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/scan" element={<QRScannerComponent />} />
