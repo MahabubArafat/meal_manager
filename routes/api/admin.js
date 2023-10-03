ecd6b53acb8f80d2e06811da72258d387bbcbbe0
MahabubArafat <arafatmahabub37@gmail.com>
2023-10-03 19:39:18 +0600

    this commit is just to stage, facing error: illegal argument for accessing api/admin/register

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const config = require("config");
 
 const adminMiddleWare = require("../../middleware/adminMiddleware");
 const StudentProfile = require("../../models/StudentProfile");
@@ -37,9 +40,13 @@ router.get("/all_profile", adminMiddleWare, async (req, res) => {
 // @route       POST api/admin/
 // @description Login as admin
 // @access      Private
+
+// @route       POST api/admin/register
+// @description Register as admin
+// @access      Private
 //TODO in future add NID card
 router.post(
-  "/",
+  "/register",
   [
     check("name", "Full Legal Name").not().isEmpty(),
     check("email", "Provide Your Email").isEmail(),
@@ -47,11 +54,56 @@ router.post(
     check("address", "Provide Legal Address").not().isEmpty(),
     check("password", "Please Provide a strong Password").isLength({ min: 6 }),
   ],
-  async (req, res) => {}
-);
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-// @route       POST api/admin/register
-// @description Register as admin
-// @access      Private
+    const { name, email, phoneNumber, address, password } = req.body;
+
+    try {
+      let admin = await Admin.findOne({ email });
+
+      if (admin) {
+        return res
+          .status(400)
+          .json({ msg: "An admin with same credentails already there" });
+      }
+
+      admin = new Admin({
+        name,
+        email,
+        phoneNumber,
+        address,
+        password,
+      });
+
+      const salt = bcrypt.genSalt(10);
+      admin.password = await bcrypt.hash(password, salt);
+
+      await admin.save();
+
+      const payload = {
+        admin: {
+          id: admin.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtAdminSecretToken"),
+        { expiresIn: 3600 },
+        (token, error) => {
+          if (error) throw error;
+          res.json(token);
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
